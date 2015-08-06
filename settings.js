59730ff17718485477d33444476407bea022ef97
Nik Nyby <nnyby@columbia.edu>
2015-08-06 13:55:45 -0400

    update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,7 @@
 var SherdBookmarkletOptions = {
-    flickr_apikey: '1234567890abcdefghijklmnopqrstuvwxyz',
-    host_url: 'https://maldive.ccnmtl.columbia.edu/save/',
-    user_url: 'https://maldive.ccnmtl.columbia.edu/accounts/logged_in.js',
+    flickr_apikey: 'fill this in',
+    youtube_apikey: 'fill this in',
+    host_url: 'https://mediathread.stage.ccnmtl.columbia.edu/save/',
+    user_url: 'https://mediathread.stage.ccnmtl.columbia.edu/accounts/logged_in.js',
     cross_origin: true
 };
