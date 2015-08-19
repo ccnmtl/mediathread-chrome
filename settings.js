83ca740d99b2a8be90e7c56124d92f2d6aec8548
Nik Nyby <nnyby@columbia.edu>
2015-08-19 10:23:08 -0400

    jshint fixes
    jshint is finally passing now. I've removed the eval() call,
    and told jshint to ignore the function in a loop that I haven't
    come up with a replacement for.

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 var MediathreadCollectOptions = {
     cross_origin: true,
     host_url: 'https://mediathread.qa.ccnmtl.columbia.edu/',
-    user_url: 'https://mediathread.qa.ccnmtl.columbia.edu/accounts/is_logged_in/',
+    user_url: 'https://mediathread.qa.ccnmtl.columbia.edu/accounts/is_logged_in/'
 };
