a4c492ee4e32ac44148cd15302794bf50c79d575
Nik Nyby <nnyby@columbia.edu>
2015-08-25 10:20:25 -0400

    use main mediathread instance

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 var MediathreadCollectOptions = {
     cross_origin: true,
-    host_url: 'https://mediathread.qa.ccnmtl.columbia.edu/',
-    user_url: 'https://mediathread.qa.ccnmtl.columbia.edu/' +
+    host_url: 'https://mediathread.ccnmtl.columbia.edu/',
+    user_url: 'https://mediathread.ccnmtl.columbia.edu/' +
         'accounts/is_logged_in/'
 };
