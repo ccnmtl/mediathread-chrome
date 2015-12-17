74ab15dfedcf7bdb0cb68b84571b237badbeadf7
Nik Nyby <nnyby@columbia.edu>
2015-12-17 13:09:53 -0500

    Introduce collectPopupClickHandler
    This lets us define different click handlers for the "Collect" click
    event so we can still share the majority of code across browser
    extensions.

--- src/background.js
+++ src/background.js
@@ -2,6 +2,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, {file: 'lib/jquery-2.1.4.min.js'});
     chrome.tabs.executeScript(tab.id, {file: 'lib/URI.js'});
     chrome.tabs.executeScript(tab.id, {file: 'src/loadcss.js'});
+    chrome.tabs.executeScript(tab.id, {file: 'src/collect-popup.js'});
     chrome.tabs.executeScript(tab.id, {file: 'src/common/settings.js'});
     chrome.tabs.executeScript(tab.id, {file: 'src/common/host-handler.js'});
     chrome.tabs.executeScript(tab.id, {file: 'src/common/asset-handler.js'});
