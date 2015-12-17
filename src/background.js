a39d4eddd0b0f6849ac7fcded57f9d0345dcf60d
CCNMTL Staff <ccnmtlstaff@dyn-160-39-237-197.dyn.columbia.edu>
2015-12-17 15:08:12 -0500

    Make host url configurable - PMT #104364
    The mediathread instance that the extension points to can now
    be chosen with a dropdown menu in the extension's options.
    
    For now, to make the UI simple, I'm just providing three pre-determined
    choices for the mediathread url: production, staging, and qa.
    
    I've done some refactoring here, removing settings.js since
    it's not really necessary.
    
    There's a bunch of changes to the common code that I will merge
    into the rest of the repositories.

--- src/background.js
+++ src/background.js
@@ -3,7 +3,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, {file: 'lib/URI.js'});
     chrome.tabs.executeScript(tab.id, {file: 'src/loadcss.js'});
     chrome.tabs.executeScript(tab.id, {file: 'src/collect-popup.js'});
-    chrome.tabs.executeScript(tab.id, {file: 'src/common/settings.js'});
     chrome.tabs.executeScript(tab.id, {file: 'src/common/host-handler.js'});
     chrome.tabs.executeScript(tab.id, {file: 'src/common/asset-handler.js'});
     chrome.tabs.executeScript(tab.id, {file: 'src/common/collect.js'});
