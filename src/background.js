6363f99bc15d190204e2471a41914cbdca601fc6
Nik Nyby <nnyby@columbia.edu>
2016-02-24 15:18:43 -0500

    Add listener for settings update button - PMT #105479

--- src/background.js
+++ src/background.js
@@ -1,3 +1,19 @@
+chrome.runtime.onMessageExternal.addListener(
+    function(request, sender, sendResponse) {
+        if (request.command && request.command === 'updatesettings') {
+            var url = sender.url;
+            url = url.replace(/#$/, '');
+            url = url.replace(/\/$/, '');
+            chrome.storage.sync.set({
+                options: {
+                    hostUrl: 'other',
+                    customUrl: url
+                }
+            }, function optionsSaved() {
+            });
+        }
+    });
+
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, {file: 'lib/jquery-2.2.0.min.js'});
     chrome.tabs.executeScript(tab.id, {file: 'lib/URI.js'});
