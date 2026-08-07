a2a64e77221d897e42903a8954e6f2d592184e8d
ndittren <36773036+ndittren@users.noreply.github.com>
2026-08-07 09:41:21 -0400

    Returning the promise, so mocha reports rejections

--- tests/host-handler.js
+++ tests/host-handler.js
@@ -15,34 +15,31 @@ describe('hosthandler', function() {
 });
 
 describe('blakearchive.org', function() {
-    it('has the expected title', function(done) {
-        JSDOM.fromURL(
+    it('has the expected title', function() {
+        return JSDOM.fromURL(
             'http://www.blakearchive.org/' +
                 'copy/songsie.n?descId=songsie.n.illbk.08').then(dom => {
             assert.equal(dom.window.document.title,
                 'The William Blake Archive');
-            done();
         });
     });
 
-    it('has the expected title for the enlarged image page', function(done) {
-        JSDOM.fromURL('http://www.blakearchive.org/' +
+    it('has the expected title for the enlarged image page', function() {
+        return JSDOM.fromURL('http://www.blakearchive.org/' +
                       'new-window/enlargement/songsie.n?' +
                       'descId=songsie.n.illbk.08')
             .then(dom => {
                 assert.equal(dom.window.document.title, 'The William Blake Archive');
-                done();
             });
     });
 });
 
 describe('classpop.ctl.columbia.edu', function() {
-    it('has the expected DOM', function(done) {
-        JSDOM.fromURL('https://classpop.ctl.columbia.edu/' +
+    it('has the expected DOM', function() {
+        return JSDOM.fromURL('https://classpop.ctl.columbia.edu/' +
                       'freedomofspeech.html')
             .then(dom => {
                 assert.ok(dom.window.document.getElementById('the-video'));
-                done();
             });
     });
 });
@@ -58,14 +55,13 @@ describe('classpop.ctl.columbia.edu', function() {
 });*/
 
 describe('youtube.com', function() {
-    it('has the expected DOM', function(done) {
-        JSDOM.fromURL('https://www.youtube.com/watch?v=ZQhbB6-cT3Y')
+    it('has the expected DOM', function() {
+        return JSDOM.fromURL('https://www.youtube.com/watch?v=ZQhbB6-cT3Y')
             .then(dom => {
                 // The host handler looks for #movie_player, but this won't
                 // get rendered in jsdom. We can look for the on-page
                 // javascript that instantiates it with this ID, though.
                 assert.ok(dom.serialize().indexOf('movie_player') >= 0);
-                done();
             });
     });
 });
